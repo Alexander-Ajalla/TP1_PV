3b31c8b1c8623fa9e8178cf255df05f0c20ba5dd
alexg <alexg@LAPTOP-AKFN76J1>
2024-04-08 19:45:29 -0300

    Creación de clase y resolución ejercicio 1

---
+++ TP1_PV/src/ar/edu/unju/fi/Ejercicio_1.java
@@ -0,0 +1,18 @@
+package ar.edu.unju.fi;
+
+public class Ejercicio_1 {
+
+	public static void main(String[] args) {
+		float num1, num2 , num3, num4 ,num5;
+		
+		num1=8;
+		num2=5;
+		num3=1;
+		num4=20;
+		num5=9;
+		
+		float promedio=(num1 + num2 + num3 + num4 + num5)/5;
+		System.out.println("El promedio es: " + promedio);
+	}
+
+}
