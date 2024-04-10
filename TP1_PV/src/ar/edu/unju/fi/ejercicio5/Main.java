453d8a79dc76947b5b6f5e8733f1ac37ec3f4a77
alexg <alexg@LAPTOP-AKFN76J1>
2024-04-10 16:42:21 -0300

    Implementación de estructura condicional ejercicio 5

--- TP1_PV/src/ar/edu/unju/fi/ejercicio5/Main.java
+++ TP1_PV/src/ar/edu/unju/fi/ejercicio5/Main.java
@@ -9,11 +9,14 @@ public class Main {
 		System.out.println("Ingrese un numero entre 1 y 9");
 		Integer num = scanner.nextInt();
 		int producto=0;
-		
+		if(num >= 1 && num <= 9) {
 		for (int i = 0;i <= 10; i++) {
 			producto = num * i;
 			System.out.println(num + " * " + i + " = " + producto );
+			}
+		} else {
+			System.out.println("ERROR - Numero fuera de rango");
 		}
+	
 	}
-
 }
