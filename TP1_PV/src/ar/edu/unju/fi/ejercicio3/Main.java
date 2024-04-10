4c582d020879c061dc7d65893521d9aa83d758da
alexg <alexg@LAPTOP-AKFN76J1>
2024-04-10 15:03:30 -0300

    Creación y resolución ejercicio 3

--- TP1_PV/src/ar/edu/unju/fi/ejercicio3/Main.java
+++ TP1_PV/src/ar/edu/unju/fi/ejercicio3/Main.java
@@ -1,22 +1,18 @@
 package ar.edu.unju.fi.ejercicio3;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 		
-		for(int i = 0; i < 5; i++) {
-			System.out.println("Valor de i es:" +i);
-		}
-		
-		int num = 0;
-		for(int i = 2; num < 20; num++) {
-			System.out.println("Valor para numero: "+num);
-			System.out.println("Valor para numero multiplicado por 2: "+num*i);
-		}
-		
-		for(int i = 0, j=20, k=2;i < j; i++, k++) {
-			System.out.println("Multiplicacion "+i+" x "+j+" = "+ i*j);
-			System.out.println("Suma "+j + " + " + k+" = "+ (j+k));
+		System.out.println("Ingrese un numero:");
+		Integer num =scanner.nextInt();
+		if(num / 2 == 0 ) {
+			System.out.println("El numero "+ num +" es par");
+		} else {
+			System.out.println("El numero "+ num + " es impar");
 		}
 	}
 
