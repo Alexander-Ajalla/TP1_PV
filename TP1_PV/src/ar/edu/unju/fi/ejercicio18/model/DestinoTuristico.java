eb0d3177a300a25d65a76839e3be37109b4b98c3
alexg <alexg@LAPTOP-AKFN76J1>
2024-04-20 23:28:56 -0300

    Creacion de paquete y resolucion de ejercicio 18

---
+++ TP1_PV/src/ar/edu/unju/fi/ejercicio18/model/DestinoTuristico.java
@@ -0,0 +1,66 @@
+package ar.edu.unju.fi.ejercicio18.model;
+
+public class DestinoTuristico {
+	private String codigo;
+	private String nombre;
+	private Integer precio;
+	private Pais pais;
+	private Integer cantDias;
+	
+	public DestinoTuristico() {}
+	
+	public DestinoTuristico(String codigo, String nombre, Integer precio, Pais pais, Integer cantDias) {
+		super();
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.precio = precio;
+		this.pais = pais;
+		this.cantDias = cantDias;
+	}
+
+	@Override
+	public String toString() {
+		return "DestinoTuristico [codigo=" + codigo + ", nombre=" + nombre + ", precio=" + precio + ", pais=" + pais
+				+ ", cantDias=" + cantDias + "]";
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Integer getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(Integer precio) {
+		this.precio = precio;
+	}
+
+	public Pais getPais() {
+		return pais;
+	}
+
+	public void setPais(Pais pais) {
+		this.pais = pais;
+	}
+
+	public Integer getCantDias() {
+		return cantDias;
+	}
+
+	public void setCantDias(Integer cantDias) {
+		this.cantDias = cantDias;
+	}
+}
